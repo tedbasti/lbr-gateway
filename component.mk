006d265cf4b12b2fcc32430b12fb28776aa2be3a
tedbasti <tedbasti@gmx.de>
2018-06-23 19:20:16 +0200

    Adding new implementation of radio/layer1
    - Adding new folder layer1 within the radio folder
    - Adapt Makefiles, so that the new folder content is compiled
    - Adding layer1.cpp/layer1.h which only contains the layer1
    - At the moment this is only layer1 for transmitting data. Receive will follow.
    - The timing unit will follow!

--- component.mk
+++ component.mk
@@ -1,11 +1,11 @@
-include $(WORKING_DIR)/basemake.mk
+all: subdirs $(SHARED_LIB) 
 
-all: $(SHARED_LIB) 
+include $(WORKING_DIR)/subdirs.mk
 
 $(SHARED_LIB): $(SHARED_OBJECTS) 
 	avr-ar crf $(SHARED_LIB) $(SHARED_OBJECTS)
 
-clean: 
+clean: cleansubdirs
 	rm $(SHARED_OBJECTS)
 	rm $(SHARED_LIB)
 	
---
+++ src/radio/layer1/layer1.cpp
@@ -0,0 +1,34 @@
+/*
+ * layer1.cpp
+ *
+ *  Created on: Jun 23, 2018
+ *      Author: ted
+ */
+
+#include "../layer1/layer1.h"
+
+#include "../bitringbuffer.h"
+#include "../checksum.h"
+#include "../../util/util.h"
+
+namespace LAYER1 {
+static BitRingBuffer<1024> ringBuf;
+
+void sendBit(bool bit) {
+	ringBuf.pushBit(bit);
+}
+
+void init_time() {
+
+}
+
+void onTimeSlice() {
+	if (ringBuf.isEmpty()) {
+		return;
+	}
+
+	bool outBit = ringBuf.popBit();
+	SET_P(DATA_OUT_PORT, DATA_OUT_PIN, outBit);
+}
+
+}
