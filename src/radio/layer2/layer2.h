361af3d985da0352986895facae86a066c4320ae
ramon-pio <mail@ramon-piotrowski.de>
2018-06-24 15:12:38 +0200

    Added folder 'layer2' and file 'layer2.h'

---
+++ src/radio/layer2/layer2.h
@@ -0,0 +1,15 @@
+/*
+ * layer2.h
+ *
+ *  Created on: Jun 24, 2018
+ *      Author: ramon-pio
+ */
+
+#ifndef SRC_RADIO_LAYER2_LAYER2_H_
+#define SRC_RADIO_LAYER2_LAYER2_H_
+
+
+
+
+
+#endif /* SRC_RADIO_LAYER2_LAYER2_H_ */
