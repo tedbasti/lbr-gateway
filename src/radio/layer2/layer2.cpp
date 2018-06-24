03859f9fd2c243ae42efe88c5121518d9f74d646
tedbasti <tedbasti@gmx.de>
2018-06-24 17:32:58 +0200

    Adding interface/Makefile for layer2
    The interface of layer2 contains two methods
    - rcvBit(bool): called by the first layer after receiving data from the physics
    - sendData(uint8_t, char*, uint8_t): called by the layer above (layer3/usart), to make a frame and encode the data (and send it to layer1)

---
+++ src/radio/layer2/layer2.cpp
@@ -0,0 +1,11 @@
+#include "layer2.h"
+
+namespace LAYER2 {
+	void rcvBit(bool bit) {
+		//TODO: implement
+	}
+
+	void sendFrame(uint8_t rcv,char* data, uint8_t len) {
+		//TODO: implement
+	}
+}
--- src/radio/layer2/layer2.h
+++ src/radio/layer2/layer2.h
@@ -1,20 +1,44 @@
 /*
  * layer2.h
  *
- *  Created on: Jun 24, 2018
- *      Author: ramon-pio
+ * This layer is there to encode stuff and so on.
+ * Look at the description of the functions
+ * - rcvBit(bool bit)
+ * - sendDate(uint8_t rcv, char* data, uint8_t len)
+ * fore more information.
+ *
+ * This layer communicates only with the
+ * layer1 or with layer3/usart interface!
  */
 
 #ifndef SRC_RADIO_LAYER2_LAYER2_H_
 #define SRC_RADIO_LAYER2_LAYER2_H_
-
+#include <stdint.h>
 
 namespace LAYER2 {
+	/*
+	 * This function is called by the lower layer (layer 1)
+	 * and rcv 1 bit.
+	 * This function is responsible for:
+	 * - Find out where the frame starts and ends (start and end sequence)
+	 * - Build a frame upon the receiving bits
+	 * - Decoding
+	 * - Check if the checksum is right
+	 * - Check if the receiver is the right one (otherwise drop)
+	 */
+	void rcvBit(bool bit);
 
-
+	/**
+	 * This function is called by the higher layer (layer3
+	 * or the usart receiver).
+	 * This function is responsible for:
+	 * - Encoding
+	 * - Checksum generation
+	 * - Build a frame with transmitterId and receiverId
+	 * (- Send start and end sequence)
+	 */
+	void sendData(uint8_t rcv, char* data, uint8_t len);
 }
-
-
 
 
 #endif /* SRC_RADIO_LAYER2_LAYER2_H_ */
