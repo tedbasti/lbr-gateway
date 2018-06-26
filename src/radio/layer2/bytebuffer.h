2da19249d08787e68e868fd7d2faf3131c652053
tedbasti <tedbasti@gmx.de>
2018-06-26 03:04:11 +0200

    Implement receive function of layer2
    It's not 100 percent finished, coz the checksum needs to be verified and the
    finished frame needs to be give to the next layer afterwards.

---
+++ src/radio/layer2/bytebuffer.h
@@ -0,0 +1,46 @@
+/*
+ * Within that ByteBuffer you can store multiple bytes.
+ * After you store them, you can get the raw data and
+ * copy them where you want.
+ * Copy could be done with
+ */
+
+#ifndef BYTEBUFFER
+#define BYTEBUFFER
+
+#include <stdint.h>
+//string.h needed for memcpy
+#include <string.h>
+//Bitringbuffer need for the typedef of BufferSize
+#include "../bitringbuffer.h"
+
+template <BufferSize Size>
+class ByteBuffer {
+private:
+	uint8_t buffer[Size];
+	//The current fill state of the buffer
+	uint16_t fill=0;
+
+public:
+	void pushByte(uint8_t byte) {
+		assert(fill < Size);
+		buffer[fill] = byte;
+		fill++;
+	}
+
+	uint8_t popByte() {
+		assert(fill > 0);
+		fill--;
+		return buffer[fill];
+	}
+
+	void clear() {
+		fill = 0;
+	}
+
+	void rawValue(uint8_t* result) {
+		memcpy(result, buffer, fill);
+	}
+};
+
+#endif
--- src/radio/layer2/layer2.cpp
+++ src/radio/layer2/layer2.cpp
@@ -3,16 +3,134 @@
 #include "../layer1/layer1.h"
 #include "frame.h"
 #include "checksum.h"
+#include "bytebuffer.h"
 
 namespace LAYER2 {
 	typedef XorChecksum8 ChecksumAlgo;
 	const uint8_t startSeq = 0xE3; // 11100011
 	const uint8_t endSeq = 0xFF; // 11111111
+	static uint8_t byte = 0;
+	static uint8_t bitsReceived = 0;
+	/*
+	 * Within the ByteBuffer a Frame is stored.
+	 * Not so much bytes are needed. 10 should work.
+	 */
+	static ByteBuffer<10> buffer;
 
-	void receiveBit(bool bit) {
-		//TODO: implement
+	/**
+	 * This is for the receive logic
+	 */
+	static bool inline receiveManchesterDecode(bool bit) {
+		/*
+		 * The byte will be stored within the pushByte,
+		 * when 8 bits are done.
+		 * bitsReceived store how much bits are received.
+		 * When the bitsReceived is odd, (the second bit
+		 * of a manchester decoding) it must differ
+		 * from the current bit to be valid.
+		 * Otherwise false is returned.
+		 */
+		static bool lastBit = 0;
+
+		//When its a odd bit
+		if (bitsReceived%2) {
+			/*
+			 * Manchester is always different!
+			 */
+			if (bit == lastBit) {
+				return false;
+			}
+		}
+		//The current value of byte will shift to the left
+		byte=byte<<1;
+		/*
+		 * We now that its a valid manchester code,
+		 * and the second one
+		 * (lastBit first received, bit afterwards)
+		 * is always the value.
+		 * So we just get the new value with byte|bit.
+		 */
+		byte=byte|bit;
+		//Count bitsReceived, AFTER check if odd
+		bitsReceived++;
+		//16 bits are one byte for manchester (16/2 = 8bits)
+		if (bitsReceived == 16) {
+			buffer.pushByte(byte);
+			byte=0;
+			bitsReceived=0;
+		}
+		//lastBit is now bit for the next round
+		lastBit=bit;
+		return true;
 	}
 
+	/*
+	 * This function should only be called with
+	 * valid frame bits!
+	 * This function decode the given bits
+	 * and give them to the "next layer" which
+	 * builds up a frame from the data.
+	 */
+	bool inline receiveBitDecode(bool bit) {
+		return receiveManchesterDecode(bit);
+	}
+
+	/*
+	 * This function takes one bit from layer1,
+	 * This function ensure that the next function
+	 * only gets bits from the real frame.
+	 */
+#define STATE_WAITING 0
+#define STATE_RECEIVING 1
+	void receiveBit(bool bit) {
+		/* Initialize this to something that can never be shifted
+		 * into a valid sequence with non-sequence bits */
+		static uint8_t seq = 0x55;
+		//Just add the new bit to the seq
+		seq = (seq << 1) | bit;
+		/*
+		 * At the beginning the state is initialized.
+		 * When a valid startsequence is received,
+		 * the state changed to receive until
+		 * the "decode layer" returns that there was
+		 * wrong bit sequence.
+		 */
+		static uint8_t currentState = STATE_WAITING;
+
+		if (currentState == STATE_RECEIVING) {
+			//Invalid Code!
+			if (!receiveBitDecode(bit)) {
+				//"Poor" version for end sequence
+				//Only check first bits
+				if ((seq & 0x3) == 0x3) {
+					//Get the checksum
+					uint8_t checksum = buffer.popByte();
+					//Generate a frame
+					Frame f;
+					buffer.rawValue((uint8_t *)&f);
+					/*
+					 * TODO: Calculate the checksum for the
+					 * frame and compare it to the received checksum.
+					 */
+				}
+				currentState = STATE_WAITING;
+			}
+		}
+		/*
+		 * When a start sequence appears,
+		 * just start from the beginning.
+		 */
+		if (seq==startSeq) {
+			buffer.clear();
+			byte=0;
+			bitsReceived=0;
+			currentState=STATE_RECEIVING;
+		}
+	}
+
+	/**
+	 * Here starts the transmit logic
+	 */
 	inline static void pushBitToLayer1(bool bit) {
 		LAYER1::sendBit(bit);
 	}
