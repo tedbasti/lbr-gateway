a422e9ca94722c34b4f2be22d4a413d269608386
ramon-pio <mail@ramon-piotrowski.de>
2018-07-12 21:49:18 +0200

    Specified layer 3 interface for sending

--- src/radio/layer3/layer3.h
+++ src/radio/layer3/layer3.h
@@ -8,6 +8,27 @@
 #ifndef SRC_RADIO_LAYER3_LAYER3_H_
 #define SRC_RADIO_LAYER3_LAYER3_H_
 
+#include <stdint.h>
+#include "databuffer.h"
+
+
+namespace LAYER3 {
+
+	/**
+	 * This function handles the layer 3 specific timing actions.
+	 */
+	void onTime();
+
+	/**
+	 * This function examinates if a certain  dataset needs to be sended.
+	 */
+	bool onHandlingNeeded(DataBuffer &databuffer);
+
+	/**
+	 * This function creates the and sends the packets that needs to be sended.
+	 */
+	bool sendData(DataBuffer &databuffer);
+}
 
 
 
