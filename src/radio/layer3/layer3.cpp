fd7b6055b722d7612806f1c9a540d22fca0ef68e
ramon-pio <mail@ramon-piotrowski.de>
2018-07-13 02:42:26 +0200

    Implemented function: receiveData(const uint8_t *data, uint8_t len)

--- src/radio/layer3/layer3.cpp
+++ src/radio/layer3/layer3.cpp
@@ -3,6 +3,12 @@
 #include "layer3.h"
 #include "../layer2/layer2.h"
 #include "../../util/configExtern.h"
+#include "../../usart/usart.h"
+
+
+namespace MAIN {
+	extern DataBuffer<TRANSMIT_BUFFER_SIZE> transmitBuffer;
+}
 
 namespace LAYER3 {
 
@@ -33,6 +39,16 @@ namespace LAYER3 {
 	}
 
 	void receiveData(const uint8_t *data, uint8_t len) {
+		switch(data[0]) {
+			case PACKET_CODE_DATA:
+				USART::transmit(data + 1, len - 1);
+				uint8_t packet[1];
+				packet[0] = PACKET_CODE_ACK;
+				LAYER2::transmitData(CONFIG::receiverId, &packet, 1);
+				break;
 
+			case PACKET_CODE_ACK:
+				transmitbuffer.popFront();
+		}
 	}
 }
