c5ae0fd40e8c29d2dc60fe13a7080e23f9455ca0
ramon-pio <mail@ramon-piotrowski.de>
2018-07-14 20:48:22 +0200

    Moved defines out of namespace

--- src/util/timerconf.h
+++ src/util/timerconf.h
@@ -3,13 +3,15 @@
 
 #include <stdint.h>
 
-namespace TIMERCONF {
-
 #define FOSC_DEF 16000000	// Quartz oscillator frequency [Hz]
 #define PRESCALER_DEF 256	// Prescaler
 #define FOC_DEF 2000		// Timer Compare Interrupt frequency [Hz]
 #define OCRNX_DEF uint16_t(FOSC_DEF/PRESCALER_DEF/FOC_DEF - 1)	// Output Compare Register Value
 
+
+namespace TIMERCONF {
+
+const uint16_t FOSC = (uint16_t) FOSC_DEF;
 const uint16_t PRESCALER = (uint16_t) PRESCALER_DEF;
 const uint16_t FOC = (uint16_t) FOC_DEF;
 const uint16_t OCRNX = OCRNX_DEF;
