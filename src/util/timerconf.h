b554119fd7c1cfd27decf64f114dc4542482dcda
ramon-pio <mail@ramon-piotrowski.de>
2018-07-14 20:37:23 +0200

    Bugfix: #define Typo

--- src/util/timerconf.h
+++ src/util/timerconf.h
@@ -5,10 +5,10 @@
 
 namespace TIMERCONF {
 
-#define FOSC_DEF 16000000							// Quartz oscillator frequency [Hz]
-#define PRESCALER_DEF 256							// Prescaler
-#define FOC_DEF 2000								// Timer Compare Interrupt frequency [Hz]
-#define OCRNX_DEF uint16_t(FOSC/PRESCALER/FOC - 1)	// Output Compare Register Value
+#define FOSC_DEF 16000000	// Quartz oscillator frequency [Hz]
+#define PRESCALER_DEF 256	// Prescaler
+#define FOC_DEF 2000		// Timer Compare Interrupt frequency [Hz]
+#define OCRNX_DEF uint16_t(FOSC_DEF/PRESCALER_DEF/FOC_DEF - 1)	// Output Compare Register Value
 
 const uint16_t PRESCALER = (uint16_t) PRESCALER_DEF;
 const uint16_t FOC = (uint16_t) FOC_DEF;
