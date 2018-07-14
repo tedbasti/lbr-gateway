055f5c9b83c4a0349b3ffede6cf1f0f4d3e57a8a
ramon-pio <mail@ramon-piotrowski.de>
2018-07-14 04:45:34 +0200

    Modified function: void setupTimer16bit(uint8_t prescaler, uint16_t frequency)

--- src/util/timer.cpp
+++ src/util/timer.cpp
@@ -14,21 +14,57 @@ void setupTimer8bit() {
 	TIMSK0 |= (1<<TOIE0);
 }
 
-// Reference:
-// https://sites.google.com/site/qeewiki/books/avr-guide/timers-on-the-atmega328
-void setupTimer16bit(uint8_t prescaler, uint16_t period) {
-	assert(prescaler <= 0x7);
-	
-	OCR1A = period;
+/**
+ * This function initializes Timer/Counter 1 in Waveform generation mode 4
+ * so that the timer compare interrupt will occure with the specified
+ * frequency.
+ */
+void setupTimer16bit(uint8_t prescaler, uint16_t frequency) {
+	bool prescalerCorrect = ((prescaler == 1)
+			|| (prescaler == 8)
+			|| (prescaler == 64)
+			|| (prescaler == 256)
+			|| (prescaler == 1024));
 
-    TCCR1B |= (1 << WGM12);
-    // Mode 4, CTC on OCR1A
+	// Assert that the prescaler value is set correctly
+	assert(prescalerCorrect);
 
-    TIMSK1 |= (1 << OCIE1A);
-    // Set interrupt on compare match
+	// Assert that the specified frequency can be established with the specified prescaler.
+	assert(1 <= (16000000/prescaler/frequency/2 - 1) <= 65535);
 
-    TCCR1B |= (prescaler << CS10);
-    // set prescaler and start timer
+	// Reset TC1 Control Register A
+	TCCR1A = 0x00;
+
+	// Initialize TC1 Counter Value Low byte and High byte
+	TCNT1L = 0x00;
+	TCNT1H = 0x00;
+
+	// Set Output Compare Register for specified frequency
+	OCR1A = (uint16_t) 16000000/prescaler/frequency/2 - 1;
+
+	// Waveform Generation Mode 4 (CTC)
+	TCCR1B |= (1 << WGM12);
+
+	// Set prescaler
+	switch(prescaler) {
+		case 1:
+			TCCR1B |= (1 << CS10);
+			break;
+		case 8:
+			TCCR1B |= (1 << CS11);
+			break;
+		case 64:
+			TCCR1B |= (1 << CS11) | (1 << CS10);
+			break;
+		case 256:
+			TCCR1B |= (1 << CS12);
+			break;
+		case 1024:
+			TCCR1B |= (1 << CS12) | (1 << CS10);
+	}
+
+	// Enable timer compare interrupt
+	TIMSK1 |= (1 << OCIE1A);
 }
 
 }
