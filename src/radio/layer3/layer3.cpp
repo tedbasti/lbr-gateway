3f983dd5d3215a1eb8e9d0bd722096cf6c8191b5
ramon-pio <mail@ramon-piotrowski.de>
2018-07-15 15:52:27 +0200

    Implemented feature packet numbers

--- src/radio/layer3/layer3.cpp
+++ src/radio/layer3/layer3.cpp
@@ -16,6 +16,7 @@ namespace LAYER3 {
 	volatile static bool timerActivated = false;
 	volatile static uint16_t timerCounter = 0;
 	static uint8_t packetNumber = 0;
+	static bool firstPacketReceived = false;
 
 	void onTime() {
 		if(timerActivated) {
@@ -34,14 +35,8 @@ namespace LAYER3 {
 	bool sendData(DataBuffer<TRANSMIT_BUFFER_SIZE> &transmitbuffer) {
 		// Create packet
 		uint8_t packet[CONFIG::payloadLen + 1];
-
-		// Create header
 		const uint8_t HEADER = (packetNumber << 4) | ((uint8_t) PACKET_CODE_DATA);
-
-		// Create payload
 		const DataSet * PAYLOAD = transmitbuffer.peekFront();
-
-		// Assemble packet
 		packet[0] = HEADER;
 		memcpy(&packet[1], PAYLOAD, CONFIG::payloadLen);
 
@@ -53,21 +48,43 @@ namespace LAYER3 {
 	}
 
 	void receiveData(const uint8_t *data, uint8_t len) {
-		switch(data[0]) {
+		// Reading header
+		const uint8_t PACKET_NUMBER = (data[0] >> 4);
+		const uint8_t PACKET_CODE = 0b00001111 & data[0];
+
+		switch(PACKET_CODE) {
 			case PACKET_CODE_DATA:
+				// Create and send ACK
 				uint8_t packet[1];
-				packet[0] = PACKET_CODE_ACK;
+				packet[0] = (PACKET_NUMBER << 4) | ((uint8_t) PACKET_CODE_ACK);
 				_delay_ms(DELAY_ACK_PACKET_MS);
 				LAYER2::transmitData(CONFIG::receiverId, packet, 1);
-				USART::transmit(data+1, len-1);
-				DEBUG_PRINT('D');
+
+				// If this is the first data packet which the slave is receiving.
+				if(firstPacketReceived == false) {
+					firstPacketReceived = true;
+					USART::transmit(data+1, len-1);
+					DEBUG_PRINT('D');
+				}
+				else {
+					// If data packet is not a duplicate
+					if(PACKET_NUMBER != packetNumber) {
+						USART::transmit(data+1, len-1);
+						packetNumber = PACKET_NUMBER;
+						DEBUG_PRINT('D');
+					}
+				}
+
 				break;
 
 			case PACKET_CODE_ACK:
-				if (!MAIN::transmitBuffer.isEmpty()) {
-					MAIN::transmitBuffer.popFront();
+				if(PACKET_NUMBER == packetNumber) {
+					packetNumber = (packetNumber + 1)%2;
+					if(!MAIN::transmitBuffer.isEmpty()) {
+						MAIN::transmitBuffer.popFront();
+						DEBUG_PRINT('A');
+					}
 				}
-				DEBUG_PRINT('A');
 				break;
 			default:
 				DEBUG_PRINT('X');
