531dce0f6c001788ee55a4989e6ae3a45637f3a7
ramon-pio <mail@ramon-piotrowski.de>
2018-07-21 11:56:05 +0200

    Increasing TIMEOUT and ACK delay due to performance and radio inteference issues

--- src/radio/layer3/layer3.h
+++ src/radio/layer3/layer3.h
@@ -11,10 +11,10 @@
 #define PACKET_CODE_DATA 0
 #define PACKET_CODE_ACK 1
 
-#define TIMEOUT (uint16_t) 400
+#define TIMEOUT (uint16_t) 800
 #define TIMEOUT_MAX_RESEND 3
 
-#define DELAY_ACK_PACKET_MS 0
+#define DELAY_ACK_PACKET_MS 200
 
 #include <stdint.h>
 #include "../databuffer.h"
