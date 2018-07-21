d5e97c6654e595e4f91075ff2391bf11b03719b0
ramon-pio <mail@ramon-piotrowski.de>
2018-07-21 13:33:17 +0200

    Added ioconfig.h

---
+++ src/ioconfig.h
@@ -0,0 +1,24 @@
+/*
+ * ioconfig.h
+ *
+ *  Created on: Jul 21, 2018
+ *      Author: rambonator
+ */
+
+#ifndef SRC_IOCONFIG_H_
+#define SRC_IOCONFIG_H_
+
+
+#define RX_PORT PORTD
+#define RX_ENABLE_PIN PD2
+
+#define TX_PORT PORTB
+#define TX_ENABLE_PIN PB4
+#define TX_DATA_PIN PB5
+
+#define LED_PORT PORTD
+#define LED_TX_PIN PD4
+#define LED_ERROR_PIN PD7
+
+
+#endif /* SRC_IOCONFIG_H_ */
