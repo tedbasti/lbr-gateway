9ef9738d48afaadde6d5ada32575e06d6c645357
ramon-pio <mail@ramon-piotrowski.de>
2018-07-21 15:42:30 +0200

    Added defines from ioconfig.h and deleted include of ioconfig.h

--- src/io.cpp
+++ src/io.cpp
@@ -5,9 +5,23 @@
  *      Author: rambonator
  */
 
+#define RX_DDR DDRD
+#define RX_PORT PIND
+#define RX_DATA_PIN PD2
+
+#define TX_DDR DDRB
+#define TX_PORT PORTB
+#define TX_ENABLE_PIN PB4
+#define TX_DATA_PIN PB5
+
+#define LED_DDR DDRD
+#define LED_PORT PORTD
+#define LED_TX_PIN PD4
+#define LED_ERROR_PIN PD7
+
+
 #include <stdint>
 #include "io.h"
-#include "ioconfig.h"
 
 
 namespace IO {
